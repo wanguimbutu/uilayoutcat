cdd61cdfc0b95e16ff17a43b4196ff87e89cef9f
wanguimbutu <92292540+wanguimbutu@users.noreply.github.com>
2022-02-03 13:16:29 +0300

    Add files via upload

---
+++ UILAYOUTANDNAVIGATIONS/app/src/main/java/com/example/uilayoutandnavigations/HOMEactivity.kt
@@ -0,0 +1,11 @@
+package com.example.uilayoutandnavigations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HOMEactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_h_o_m_eactivity)
+    }
+}
---
+++ UILAYOUTANDNAVIGATIONS/app/src/main/java/com/example/uilayoutandnavigations/studentsactivity.kt
@@ -0,0 +1,11 @@
+package com.example.uilayoutandnavigations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class studentsactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_studentsactivity)
+    }
+}
